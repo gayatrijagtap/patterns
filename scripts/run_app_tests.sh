38fc79c729f19e4e7e9a0a55876e57b36c498ef7
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-22 19:48:22 +0530

    Bug fix:app tests can now be run from project root

--- scripts/run_app_tests.sh
+++ scripts/run_app_tests.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-./run_test.sh create_Rectangle.js test/allRectangleInputs test/allRectangleOutputs
-./run_test.sh create_Triangle.js test/allTriangleInputs test/allTriangleOutputs
-./run_test.sh create_Diamond.js test/allDiamondInputs test/allDiamondOutputs
+./run_test.sh ../create_rectangle.js ../app_test_data/allRectangleInputs ../app_test_data/allRectangleOutputs
+./run_test.sh ../create_triangle.js ../app_test_data/allTriangleInputs ../app_test_data/allTriangleOutputs
+./run_test.sh ../create_diamond.js ../app_test_data/allDiamondInputs ../app_test_data/allDiamondOutputs
