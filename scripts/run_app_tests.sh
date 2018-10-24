3fc9f5c9ae5dcbc675eff0cb52cd24f5a8fabc62
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-10-24 19:59:14 +0530

    app tests now work from root

--- scripts/run_app_tests.sh
+++ scripts/run_app_tests.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-./run_test.sh ../create_rectangle.js ../app_test_data/allRectangleInputs ../app_test_data/allRectangleOutputs
-./run_test.sh ../create_triangle.js ../app_test_data/allTriangleInputs ../app_test_data/allTriangleOutputs
-./run_test.sh ../create_diamond.js ../app_test_data/allDiamondInputs ../app_test_data/allDiamondOutputs
+sh scripts/run_test.sh create_rectangle.js app_test_data/allRectangleInputs app_test_data/allRectangleOutputs
+sh scripts/run_test.sh create_triangle.js app_test_data/allTriangleInputs app_test_data/allTriangleOutputs
+sh scripts/run_test.sh create_diamond.js app_test_data/allDiamondInputs app_test_data/allDiamondOutputs
